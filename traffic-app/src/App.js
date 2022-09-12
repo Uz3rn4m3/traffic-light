33b3db002386f944044d05da27d66742d677ec5e
Zach Wright <zwright1090@gmail.com>
2022-09-12 11:25:31 -0500

    traffic-app

---
+++ traffic-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React, { useState } from 'react';
+import Light from './components/Light';
+
+function App() {
+  const colors =['red', 'yellow', 'green'];
+  const [lit, setLit] = useState('red');
+  return (
+    <div className="App">
+      {colors.map((color) => {
+        return <Light color={color} lit={lit} setLit={setLit}/>;
+      })}
+    </div>
+  );
+}
+
+export default App;
